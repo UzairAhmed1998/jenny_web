079e6c87bfaa5f53b4a4f5487f556d7b3269af2d
UzairAhmed1998 <122508259+UzairAhmed1998@users.noreply.github.com>
2024-02-29 19:29:17 +0500

    Delete user_web directory

--- user_web/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con = mysqli_connect("localhost","root","","ebook");
-if(!$con){
-    echo "connection failed";
-}
-session_start();
-
-?>
--- user_web/log_out.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'config.php';
-
-session_unset();
-session_destroy();
-echo "<script>window.location.href='http://localhost/jenny_web/user_web/login.php'</script>"
-
-?>
